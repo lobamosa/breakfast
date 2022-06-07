b08a1bcb0072bd41d26e97960b452a8a5868ec40
namos <lobamo@salocina.com>
2022-06-07 08:46:37 +0200

    routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'events#index'
+  post '/events/:accept', to: 'events#accept'
+  resources :events
+  resources :contributors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
