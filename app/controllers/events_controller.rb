09bf2a4cb4610444d789fb8059c60946ac947246
namos <lobamo@salocina.com>
2022-06-07 08:45:00 +0200

    eventsController with few methods index show and create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,17 @@
 class EventsController < ApplicationController
-  def index
+  def initialize()
+    @eventService = EventService::EventService.new
   end
+  def index
+    @events = Event.all
+    render json: @events
+  end
+  def show
+    @event = @eventService.get_event_by_id(params[:id])
+    render json: @event
+  end
+  def create
+    @eventService.register_an_event(params[:place], params[:title])
+  end
+
 end
