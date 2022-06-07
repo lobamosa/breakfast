3a131741bc6b21538f54e931b867cac931e973b9
namos <lobamo@salocina.com>
2022-06-07 08:47:20 +0200

    migration file with createEvent

--- db/migrate/20220605131531_create_events.rb
+++ db/migrate/20220605131531_create_events.rb
@@ -1,9 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[7.0]
   def change
     create_table :events do |t|
+      t.string :title
       t.string :place
-      t.integer :contributor
-
+      t.integer :number_of_contributor
+      t.integer :available_food
       t.timestamps
     end
   end
